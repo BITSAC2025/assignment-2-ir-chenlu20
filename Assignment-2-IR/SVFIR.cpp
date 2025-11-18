dc9429308d5ab9539b02a1d3d0bd873b9b3d3e32
chenlu20 <chen181396@163.com>
2025-11-18 11:31:23 +0800

    Update SVFIR.cpp

--- Assignment-2-IR/SVFIR.cpp
+++ Assignment-2-IR/SVFIR.cpp
@@ -1,8 +1,90 @@
+// // /**
+// //  * SVFIR.cpp
+// //  * @author 3220252746
+// //  */
+
+// // #include "Graphs/SVFG.h"
+// // #include "SVF-LLVM/SVFIRBuilder.h"
+// // #include "WPA/Andersen.h"
+
+// // using namespace SVF;
+// // using namespace llvm;
+// // using namespace std;
+
+// // int main(int argc, char **argv)
+// // {
+// //     int arg_num = 0;
+// //     int extraArgc = 4;
+// //     char **arg_value = new char *[argc + extraArgc];
+// //     for (; arg_num < argc; ++arg_num)
+// //     {
+// //         arg_value[arg_num] = argv[arg_num];
+// //     }
+// //     std::vector<std::string> moduleNameVec;
+
+// //     int orgArgNum = arg_num;
+// //     arg_value[arg_num++] = (char *)"-model-arrays=true";
+// //     arg_value[arg_num++] = (char *)"-pre-field-sensitive=false";
+// //     arg_value[arg_num++] = (char *)"-model-consts=true";
+// //     arg_value[arg_num++] = (char *)"-stat=false";
+// //     assert(arg_num == (orgArgNum + extraArgc) && "more extra arguments? Change the value of extraArgc");
+
+// //     moduleNameVec = OptionBase::parseOptions(arg_num, arg_value, "SVF IR", "[options] <input-bitcode...>");
+
+// //     LLVMModuleSet::getLLVMModuleSet()->buildSVFModule(moduleNameVec);
+
+// //     // Instantiate an SVFIR builder
+// //     SVFIRBuilder builder;
+// //     cout << "Generating SVFIR(PAG), call graph and ICFG ..." << endl;
+
+// //     // // // TODO: here, generate SVFIR(PAG), call graph and ICFG, and dump them to files
+   
+
+// //     // // 1) 建 SVFIR / PAG
+// //     // SVFIR* svfir = builder.build();
+
+// //     // // 2) 导出 SVFIR / PAG
+// //     // svfir->dump("svfir");
+
+// //     // // 3) 取 ICFG 并导出
+// //     // ICFG* icfg = svfir->getICFG();
+// //     // icfg->dump("icfg");
+
+
+// //     // // 4) 构造调用图并导出
+// //     // const CallGraph* cg = svfir->getCallGraph();
+// //     // cg->dump("callgraph");
+
+// //     // //@}
+
+// //     // TODO: here, generate SVFIR(PAG), call graph and ICFG, and dump them to files
+// //         // TODO 部分修改为：
+// //     // TODO: here, generate SVFIR(PAG), call graph and ICFG, and dump them to files
+// //     //@{
+
+// //    // 1) 建 SVFIR / PAG
+// //     SVFIR* svfir = builder.build();
+
+// //     // 2) 导出 SVFIR / PAG
+// //     svfir->dump();
+
+// //     // 3) 取 ICFG 并导出
+// //     ICFG* icfg = svfir->getICFG();
+// //     icfg->dump();
+
+// //     // 4) 通过指针分析构造调用图并导出
+// //     Andersen* pta = AndersenWaveDiff::createAndersenWaveDiff(svfir);
+// //     pta->getCallGraph()->dump();
+
+// //     //@}
+
+// //     return 0;
+// // }
+
 // /**
 //  * SVFIR.cpp
 //  * @author 3220252746
 //  */
-
 // #include "Graphs/SVFG.h"
 // #include "SVF-LLVM/SVFIRBuilder.h"
 // #include "WPA/Andersen.h"
@@ -37,50 +119,43 @@
 //     SVFIRBuilder builder;
 //     cout << "Generating SVFIR(PAG), call graph and ICFG ..." << endl;
 
-//     // // // TODO: here, generate SVFIR(PAG), call graph and ICFG, and dump them to files
+//     // TODO: here, generate SVFIR(PAG), call graph and ICFG, and dump them to files
+//     //@{
+//    // TODO: here, generate SVFIR(PAG), call graph and ICFG, and dump them to files
+//    //@{
    
-
+//    // 1) 建 SVFIR / PAG
+//    SVFIR* svfir = builder.build();
+   
+//    // 2) 运行 Andersen 分析
+//    Andersen* pta = AndersenWaveDiff::createAndersenWaveDiff(svfir);
+   
+//    // 3) dump 所有
+//    svfir->dump();
+//    pta->getICFG()->dump();  // 从 pta 获取 ICFG
+//    pta->getCallGraph()->dump();
+   
+//    //@}
+    
 //     // // 1) 建 SVFIR / PAG
 //     // SVFIR* svfir = builder.build();
 
 //     // // 2) 导出 SVFIR / PAG
-//     // svfir->dump("svfir");
+//     // svfir->dump();
 
 //     // // 3) 取 ICFG 并导出
 //     // ICFG* icfg = svfir->getICFG();
-//     // icfg->dump("icfg");
+//     // icfg->dump();
 
-
-//     // // 4) 构造调用图并导出
-//     // const CallGraph* cg = svfir->getCallGraph();
-//     // cg->dump("callgraph");
-
-//     // //@}
-
-//     // TODO: here, generate SVFIR(PAG), call graph and ICFG, and dump them to files
-//         // TODO 部分修改为：
-//     // TODO: here, generate SVFIR(PAG), call graph and ICFG, and dump them to files
-//     //@{
-
-//    // 1) 建 SVFIR / PAG
-//     SVFIR* svfir = builder.build();
-
-//     // 2) 导出 SVFIR / PAG
-//     svfir->dump();
-
-//     // 3) 取 ICFG 并导出
-//     ICFG* icfg = svfir->getICFG();
-//     icfg->dump();
-
-//     // 4) 通过指针分析构造调用图并导出
-//     Andersen* pta = AndersenWaveDiff::createAndersenWaveDiff(svfir);
-//     pta->getCallGraph()->dump();
+//     // // 4) 通过指针分析构造调用图并导出
+//     // Andersen* pta = AndersenWaveDiff::createAndersenWaveDiff(svfir);
+//     // pta->getCallGraph()->dump();
+   
 
 //     //@}
 
 //     return 0;
 // }
-
 /**
  * SVFIR.cpp
  * @author 3220252746
@@ -121,37 +196,21 @@ int main(int argc, char **argv)
 
     // TODO: here, generate SVFIR(PAG), call graph and ICFG, and dump them to files
     //@{
-   // TODO: here, generate SVFIR(PAG), call graph and ICFG, and dump them to files
-   //@{
-   
-   // 1) 建 SVFIR / PAG
-   SVFIR* svfir = builder.build();
-   
-   // 2) 运行 Andersen 分析
-   Andersen* pta = AndersenWaveDiff::createAndersenWaveDiff(svfir);
-   
-   // 3) dump 所有
-   svfir->dump();
-   pta->getICFG()->dump();  // 从 pta 获取 ICFG
-   pta->getCallGraph()->dump();
-   
-   //@}
     
-    // // 1) 建 SVFIR / PAG
-    // SVFIR* svfir = builder.build();
-
-    // // 2) 导出 SVFIR / PAG
-    // svfir->dump();
-
-    // // 3) 取 ICFG 并导出
-    // ICFG* icfg = svfir->getICFG();
-    // icfg->dump();
-
-    // // 4) 通过指针分析构造调用图并导出
-    // Andersen* pta = AndersenWaveDiff::createAndersenWaveDiff(svfir);
-    // pta->getCallGraph()->dump();
-   
-
+    // 1) 建 SVFIR / PAG
+    SVFIR* svfir = builder.build();
+    
+    // 2) 导出 SVFIR/PAG - 使用默认参数会生成 .pag 文件
+    svfir->dump("pag");  // 指定名称为"pag"
+    
+    // 3) 从SVFIR获取ICFG并导出
+    ICFG* icfg = svfir->getICFG();
+    icfg->dump("icfg");  // 指定名称为"icfg"
+    
+    // 4) 从SVFIR获取CallGraph并导出
+    CallGraph* cg = svfir->getCallGraph();  
+    cg->dump("callgraph");  // 指定名称为"callgraph"
+    
     //@}
 
     return 0;
